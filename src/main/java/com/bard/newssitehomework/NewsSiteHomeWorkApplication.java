e1fe771d129252ed520c409a8743d2e83ca714b1
bard <bartoszslesar@gmail.com>
2024-04-27 18:27:05 +0200

    project init

---
+++ src/main/java/com/bard/newssitehomework/NewsSiteHomeWorkApplication.java
@@ -0,0 +1,13 @@
+package com.bard.newssitehomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsSiteHomeWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsSiteHomeWorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bard/newssitehomework/NewsSiteHomeWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bard.newssitehomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsSiteHomeWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
